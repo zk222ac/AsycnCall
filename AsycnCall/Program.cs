d52d24482d3539dc67d0997c0a09473fa12b614d
zuhair khan <zk222ac@gmail.com>
2018-11-18 01:24:38 +0100

    final Release

--- AsycnCall/Program.cs
+++ AsycnCall/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsycnCall
@@ -10,12 +12,57 @@ namespace AsycnCall
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Program Download.......");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // call Main Thread 
+            
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.WriteLine("Main thread is....... : " + i);
+                    Thread.Sleep(500);
+                }
+            });
+            Task.Run(() =>
+            {
+               DownloadFile();
+            });
+
+            // call worker thread 1.................
+            Thread thread1 = new Thread(WorkerThreadMethod1);
+            thread1.Start();
+
+            // call worker thread 2..................
+            Thread thread2 = new Thread(WorkerThreadMethod2);
+            thread2.Start();
+
+            Console.ReadKey();
         }
+
+        static void WorkerThreadMethod1()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+               Console.WriteLine("Worker Thread 1........ : " + i);
+                Thread.Sleep(500);
+            }
+        }
+        static void WorkerThreadMethod2()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("Worker Thread 2......... : " + i);
+            }
+        }
+
+        static async void DownloadFile()
+        {
+            HttpClient client  = new HttpClient();
+            var data = await client.GetStringAsync("http://www.gmail.com");
+            Console.WriteLine("Download is complete Now:" + data);
+        }
+
+
     }
 }
